f4e64e59a39ee523215f38d73dc9defd31c2ce16
ClemThierry <49915513+ClemThierry@users.noreply.github.com>
2021-05-14 12:11:01 +0200

    Ajout fichier authentification

--- profils/connexion.php
+++ profils/connexion.php
@@ -4,7 +4,14 @@
     include_once "../functions/profils.php";
 ?>
 <main>
-    <h1>Blablabla</h1>
+    <h1>Connexion</h1>
+    <form action="authentification.php" method="post">
+        <label for="id">Identifiant :</label>
+        <input type="text" name="id"><br>
+        <label for="nom">Mot de passe :</label>
+        <input type="text" name="nom"><br>
+        <button type="submit" name="envoyer">Connexion</button>
+    </form>
 
     <p>Pas encore de compte ? <a href="inscription.php">S'inscrire</a></p>
     <a href="../index.php"><button>Retour à l'accueil</button></a> 
