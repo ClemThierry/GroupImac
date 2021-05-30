fd977e01f5fa9bd437d8c469f41a29caf484e27e
Clémence <clemvoegele@gmail.com>
2021-05-30 09:45:59 +0200

    Mini modifs

--- View/index.php
+++ View/index.php
@@ -7,9 +7,9 @@
     <main>
         <h2>Group'IMAC ? C'est quoi ?</h2>
 
-        <p>Lorem ipsum dolor explicatif. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>A l’IMAC, les travaux de groupe sont légion, et il est parfois difficile de trouver des partenaires de projet, surtout dans ce contexte difficile. En effet, les interactions entre les étudiants sont réduites du fait des cours à distance et du peu de présence sur le campus, et ils n’ont pas forcément eu le temps de tisser des liens avec leurs camarades de promotion. On se connaît moins, on évalue moins facilement les compétences des autres… Ainsi, la formation de groupe devient ardue. C’est pour ces raisons que nous avons décidé de créer Group’IMAC ! </p>
 
-        <p>Lorem ipsum dolor explicatif. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Ce site a pour vocation de fonctionner à la façon d’un panneau de “petites annonces”. Un projet à réaliser, mais personne d’autre pour le faire ? Il suffira de poster une annonce détaillant l’idée, les membres du groupe actuel s’il y en a, la catégorie, le contexte (personnel, professionnel, scolaire) du projet… Tout le monde pourra consulter ces annonces, y répondre dans les commentaires, être ajouté aux équipes du projet… Ainsi, lancer un travail de groupe devient beaucoup plus facile avec Group’IMAC ! </p>
 
         <iframe src="https://drive.google.com/file/d/1rfih7WsCOPT0QkRWlsjr2TD9rpmwwHCr/preview" width="720" height="480"></iframe>
 
